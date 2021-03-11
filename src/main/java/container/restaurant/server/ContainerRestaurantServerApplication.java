cdfd4187b86b302c90bb971442de5a512a8df705
cenibee <ksj3452@naver.com>
2021-03-11 21:26:16 +0900

    :tada: 프로젝트 초기화

---
+++ src/main/java/container/restaurant/server/ContainerRestaurantServerApplication.java
@@ -0,0 +1,13 @@
+package container.restaurant.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContainerRestaurantServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContainerRestaurantServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/container/restaurant/server/ContainerRestaurantServerApplicationTests.java
@@ -0,0 +1,13 @@
+package container.restaurant.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContainerRestaurantServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
