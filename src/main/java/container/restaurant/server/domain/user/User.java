698701f4febdd4d46791abb83e296be53a30982c
Cenibee <30590761+Cenibee@users.noreply.github.com>
2021-04-17 23:01:11 +0900

    :sparkles: 유저 도메인 기반 구현
    유저 도메인, Repository 추가

---
+++ src/main/java/container/restaurant/server/domain/user/User.java
@@ -0,0 +1,57 @@
+package container.restaurant.server.domain.user;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    private String profile;
+
+    private String greeting;
+
+    private Integer level;
+
+    private Integer experience;
+
+    private Integer feedCount;
+
+    private Integer scrapCount;
+
+    private Integer bookmarkedCount;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    private Boolean banned;
+
+    @Builder
+    protected User(String email, String profile) {
+        this.email = email;
+        this.nickname = "";
+        this.profile = profile;
+        this.greeting = null;
+        this.level = 1;
+        this.experience = 0;
+        this.feedCount = 0;
+        this.scrapCount = 0;
+        this.bookmarkedCount = 0;
+        this.role = Role.USER;
+        this.banned = false;
+    }
+}
---
+++ src/main/java/container/restaurant/server/domain/user/UserRepository.java
@@ -0,0 +1,6 @@
+package container.restaurant.server.domain.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
