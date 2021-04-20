0cd43beda955912308aaca7d1f0022a2e8b44a82
Cenibee <30590761+Cenibee@users.noreply.github.com>
2021-04-21 00:01:25 +0900

    :memo: 엔티티 공통 부분 주석 추가

--- src/main/java/container/restaurant/server/domain/base/BaseCreatedTimeEntity.java
+++ src/main/java/container/restaurant/server/domain/base/BaseCreatedTimeEntity.java
@@ -1,13 +1,21 @@
-package container.restaurant.server.domain;
+package container.restaurant.server.domain.base;
 
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
+/**
+ * {@link Entity} 클래스의 기반 클래스
+ * <p>
+ * {@link Long} 타입의 <i>id</i>, {@link LocalDateTime} 타입의 <i>createdDate</i> 속성을 제공한다.
+ * <p>
+ * 각 속성에 대해 {@code getter()} 를 제공한다.
+ */
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
