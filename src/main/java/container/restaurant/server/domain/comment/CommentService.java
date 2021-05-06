05f77c541e39159fb09987873585706119ab25a4
HongYeseul <ghd8119@naver.com>
2021-05-06 22:23:07 +0900

    :hammer: Comment 기반 코드 추가

---
+++ src/main/java/container/restaurant/server/domain/comment/CommentService.java
@@ -0,0 +1,27 @@
+package container.restaurant.server.domain.comment;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class CommentService {
+    private final CommentRepository commentRepository;
+
+    @Transactional
+    public Comment createComment(Comment comment){
+        return commentRepository.save(comment);
+    }
+
+    public <List>Comment findAllByFeed(){
+        // TODO
+        return null;
+    }
+
+    @Transactional
+    public void deleteById(Long id){ commentRepository.deleteById(id); }
+
+    public void update(){}
+}
