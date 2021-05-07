226065b5d8a281fdda6b28e253c651ac41b122cc
HongYeseul <ghd8119@naver.com>
2021-05-08 07:39:12 +0900

    🚧 CommentService, Controller 제작(Post, Get x)

--- src/main/java/container/restaurant/server/domain/comment/CommentService.java
+++ src/main/java/container/restaurant/server/domain/comment/CommentService.java
@@ -1,9 +1,14 @@
 package container.restaurant.server.domain.comment;
 
+import container.restaurant.server.domain.exception.ResourceNotFoundException;
+import container.restaurant.server.domain.feed.Feed;
+import container.restaurant.server.web.dto.comment.CommentUpdateDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -15,12 +20,18 @@ public class CommentService {
         return commentRepository.save(comment);
     }
 
-//    public <List>Comment findAllByFeed(){
-//
-//    }
+    @Transactional
+    public List<Comment> findAllByFeed(Feed feed){
+        return commentRepository.findAllByFeed(feed);
+    }
 
     @Transactional
     public void deleteById(Long id){ commentRepository.deleteById(id); }
 
-    public void update(){}
+    @Transactional
+    public void update(Long id, CommentUpdateDto commentUpdateDto){
+        Comment comment = commentRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("존재하지 않는 댓글입니다.(id:"+id+")"));
+        commentUpdateDto.updateComment(comment);
+    }
 }
---
+++ src/main/java/container/restaurant/server/web/CommentController.java
@@ -0,0 +1,50 @@
+package container.restaurant.server.web;
+
+import container.restaurant.server.domain.comment.Comment;
+import container.restaurant.server.domain.comment.CommentService;
+import container.restaurant.server.web.dto.comment.CommentUpdateDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+@Validated
+@RequestMapping("/api/comment")
+public class CommentController {
+    private final CommentService commentService;
+
+//    @PostMapping("{feedId}")
+//    public ResponseEntity<?> createComment(
+//            @PathVariable Long feedId,
+//            @RequestBody Comment comment
+//    ){
+//
+//    }
+//
+//    @GetMapping("{feedId}")
+//    public ResponseEntity<?> getCommentByFeed(
+//            @PathVariable Long feedId
+//    ){
+//
+//    }
+
+    @PatchMapping("{id}")
+    public ResponseEntity<?> updateCommentById(
+            @PathVariable Long id,
+            @RequestBody CommentUpdateDto updateDto
+    ){
+        commentService.update(id, updateDto);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<?> deleteCommentById(
+            @PathVariable Long id
+    ){
+        commentService.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+}
