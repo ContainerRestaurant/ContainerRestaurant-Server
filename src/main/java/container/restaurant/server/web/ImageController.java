775439862bf317336fd59d7387ae8494f6a8ab55
parkjung1129 <dlwfp94@gmail.com>
2021-05-07 17:03:47 +0900

    :sparkles: 이미지 업로드 구현
    사용자에게 이미지를 받아 파일서버에 올리고,
    나온 정보를 DB 저장 후 사용자에게 정보 전달

--- src/main/java/container/restaurant/server/web/ImageController.java
+++ src/main/java/container/restaurant/server/web/ImageController.java
@@ -1,14 +1,15 @@
 package container.restaurant.server.web;
 
-import container.restaurant.server.domain.image.Image;
-import container.restaurant.server.domain.image.ImageService;
+import container.restaurant.server.domain.feed.picture.Image;
+import container.restaurant.server.domain.feed.picture.ImageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.hateoas.EntityModel;
-import org.springframework.hateoas.mediatype.hal.HalModelBuilder;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
@@ -21,10 +22,17 @@ public class ImageController {
     final private ImageService imageService;
 
     @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<?> upload(@RequestBody MultipartFile image) {
+    public ResponseEntity<?> upload(@RequestBody MultipartFile image) throws IOException {
         Image imageInfo = imageService.upload(image);
         return ResponseEntity.ok(EntityModel.of(imageInfo)
-                .add(linkTo(getController().upload(image)).withSelfRel()));
+                .add(linkTo(getController().upload(image)).withSelfRel())
+                .add(linkTo(getController().upload(image)).slash(imageInfo.getUrl()).withRel("image_url")));
+    }
+
+    @GetMapping(value = "{path}")
+    public ResponseEntity<?> getImageFile() {
+
+        return null;
     }
 
     private ImageController getController() {
