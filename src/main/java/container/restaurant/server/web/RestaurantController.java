1b0be47f3cf31af3f6ad0163c439045fb893a944
dlwfp <dlwfp94@gmail.com>
2021-05-10 23:36:26 +0900

    :art: 식당 주변 검색 응답형식 변경
    식당 주변 검색 결과 CollectionModel 방식으로 변경

--- src/main/java/container/restaurant/server/web/RestaurantController.java
+++ src/main/java/container/restaurant/server/web/RestaurantController.java
@@ -1,18 +1,14 @@
 package container.restaurant.server.web;
 
-import com.fasterxml.jackson.annotation.JsonIdentityInfo;
-import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import container.restaurant.server.domain.restaurant.Restaurant;
 import container.restaurant.server.service.RestaurantService;
-import container.restaurant.server.web.dto.user.NicknameExistsDto;
 import javassist.NotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
-import org.springframework.hateoas.mediatype.hal.HalModelBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.swing.text.html.parser.Entity;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -49,20 +45,20 @@ public class RestaurantController {
     }
 
     @GetMapping("/{lat}/{lon}/{radius}")
-    @JsonIdentityInfo(generator = ObjectIdGenerators.IntSequenceGenerator.class)
-    public List<ResponseEntity<?>> findNearByRestaurants(
+    public ResponseEntity<CollectionModel<?>> findNearByRestaurants(
             @PathVariable("lat") double lat,
             @PathVariable("lon") double lon,
             @PathVariable("radius") long radius) {
         /*
             사용자 정보를 PathVariable 형태로 받는 형태로 구현 추후 입력방식 변경 가능
          */
-        return
-                restaurantService.findNearByRestaurants(lat, lon, radius).stream().map(restaurant ->
-                        ResponseEntity.ok(EntityModel.of(restaurant)
-                                .add(linkTo(getController().findById(restaurant.getId())).withRel("restaurant-info"))
-                                .add(linkTo(getController().findNearByRestaurants(lat, lon, radius)).withSelfRel()))
-                ).collect(Collectors.toList());
+
+        return ResponseEntity.ok(new CollectionModel<>(restaurantService.findNearByRestaurants(lat, lon, radius)
+                .stream().map(restaurant -> EntityModel.of(restaurant)
+                        .add(linkTo(getController().findById(restaurant.getId())).withRel("restaurant-info"))
+                        .add(linkTo(ImageController.class).slash(restaurant.getImage_path()).withRel("image-url"))
+                ).collect(Collectors.toList()))
+                .add(linkTo(getController().findNearByRestaurants(lat, lon, radius)).withSelfRel()));
     }
 
     private RestaurantController getController() {
