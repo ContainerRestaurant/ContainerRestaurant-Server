63ff9dac422a2a373fc2d77519b8781b4696eece
parkjung1129 <dlwfp94@gmail.com>
2021-05-10 15:08:44 +0900

    :art: 식당 리포제토리 위치 변경

--- src/main/java/container/restaurant/server/domain/restaurant/RestaurantRepository.java
+++ src/main/java/container/restaurant/server/domain/restaurant/RestaurantRepository.java
@@ -1,6 +1,16 @@
 package container.restaurant.server.domain.restaurant;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
+
+    @Query(nativeQuery = true,
+            value = "SELECT *, ST_DISTANCE_SPHERE(POINT(?2, ?1), POINT(longitude,latitude)) AS dist\n" +
+                    "FROM tb_restaurant FORCE INDEX FOR JOIN (`restaurant-loc-index`)\n" +
+                    "WHERE MBRCONTAINS(ST_LINESTRINGFROMTEXT( getDiagonal(?1,?2,?3)), location)")
+    List<Restaurant> findNearByRestaurants(double lat, double lon, long radius);
+
 }
