20f7c8d8f9832ede26c094642757dee83621a215
dlwfp <dlwfp94@gmail.com>
2021-05-10 23:35:43 +0900

    :sparkles: 식당 주변 검색 DTO 추가
    식당 DTO 사용 방식 추가

--- src/main/java/container/restaurant/server/service/RestaurantService.java
+++ src/main/java/container/restaurant/server/service/RestaurantService.java
@@ -1,17 +1,22 @@
 package container.restaurant.server.service;
 
+import container.restaurant.server.domain.feed.picture.Image;
+import container.restaurant.server.domain.feed.picture.ImageRepository;
 import container.restaurant.server.domain.restaurant.Restaurant;
 import container.restaurant.server.domain.restaurant.RestaurantRepository;
+import container.restaurant.server.web.dto.restaurant.RestaurantNearInfoDto;
 import javassist.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class RestaurantService {
     private final RestaurantRepository restaurantRepository;
+    private final ImageRepository imageRepository;
 
     public List<Restaurant> findAll() {
         return restaurantRepository.findAll();
@@ -23,7 +28,10 @@ public class RestaurantService {
                 .orElseThrow(() -> new NotFoundException("{" + id + "}에 해당하는 식당이 없습니다."));
     }
 
-    public List<Restaurant> findNearByRestaurants(double lat, double lon, long radius) {
-        return restaurantRepository.findNearByRestaurants(lat, lon, radius);
+    public List<RestaurantNearInfoDto> findNearByRestaurants(double lat, double lon, long radius) {
+        return restaurantRepository.findNearByRestaurants(lat, lon, radius)
+                .stream()
+                .map(RestaurantNearInfoDto::from)
+                .collect(Collectors.toList());
     }
 }
