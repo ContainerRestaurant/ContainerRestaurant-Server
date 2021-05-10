cf9c344f0fa43626f7a9ce81a4a4c2e1de52f210
parkjung1129 <dlwfp94@gmail.com>
2021-05-10 15:07:51 +0900

    :label: 식당 컨트롤러 일부 변경
    위도, 경도 파라미터 float -> double
    
    그외 응답 테스트 변경 진행 중

--- src/main/java/container/restaurant/server/web/RestaurantController.java
+++ src/main/java/container/restaurant/server/web/RestaurantController.java
@@ -1,15 +1,18 @@
-package container.restaurant.server.controller;
+package container.restaurant.server.web;
 
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import container.restaurant.server.domain.restaurant.Restaurant;
 import container.restaurant.server.service.RestaurantService;
+import container.restaurant.server.web.dto.user.NicknameExistsDto;
 import javassist.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.mediatype.hal.HalModelBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.swing.text.html.parser.Entity;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -18,18 +21,18 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/v1/restaurant/")
+@RequestMapping("/api/restaurant/")
 public class RestaurantController {
 
     private final RestaurantService restaurantService;
 
     @GetMapping
-    public List<EntityModel<?>> findAll() {
-        return restaurantService.findAll().stream().map(restaurant -> {
-            EntityModel<?> entityModel = EntityModel.of(restaurant)
-                    .add(linkTo(methodOn(RestaurantController.class).findById(restaurant.getId())).withSelfRel());
-            return entityModel;
-        }).collect(Collectors.toList());
+    public List<ResponseEntity<?>> findAll() {
+        return restaurantService.findAll().stream().map(restaurant ->
+                ResponseEntity.ok().body(
+                        EntityModel.of(restaurant)
+                                .add(linkTo(getController().findById(restaurant.getId())).withSelfRel()))
+        ).collect(Collectors.toList());
     }
 
     @GetMapping("{id}")
@@ -47,18 +50,19 @@ public class RestaurantController {
 
     @GetMapping("/{lat}/{lon}/{radius}")
     @JsonIdentityInfo(generator = ObjectIdGenerators.IntSequenceGenerator.class)
-    public List<EntityModel<?>> findNearByRestaurants(
-            @PathVariable("lat") float lat,
-            @PathVariable("lon") float lon,
+    public List<ResponseEntity<?>> findNearByRestaurants(
+            @PathVariable("lat") double lat,
+            @PathVariable("lon") double lon,
             @PathVariable("radius") long radius) {
         /*
             사용자 정보를 PathVariable 형태로 받는 형태로 구현 추후 입력방식 변경 가능
          */
-        return restaurantService.findNearByRestaurants(lat, lon, radius).stream().map(restaurant -> {
-            EntityModel<?> entityModel = EntityModel.of(restaurant)
-                    .add(linkTo(methodOn(RestaurantController.class).findById(restaurant.getId())).withSelfRel());
-            return entityModel;
-        }).collect(Collectors.toList());
+        return
+                restaurantService.findNearByRestaurants(lat, lon, radius).stream().map(restaurant ->
+                        ResponseEntity.ok(EntityModel.of(restaurant)
+                                .add(linkTo(getController().findById(restaurant.getId())).withRel("restaurant-info"))
+                                .add(linkTo(getController().findNearByRestaurants(lat, lon, radius)).withSelfRel()))
+                ).collect(Collectors.toList());
     }
 
     private RestaurantController getController() {
