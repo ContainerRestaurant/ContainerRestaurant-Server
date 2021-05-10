cfc7da6fd407dcee11e7bd8939dd3fa6dbca604f
dlwfp <dlwfp94@gmail.com>
2021-05-10 23:34:19 +0900

    :art: 식당 도메인 수정
    image_id -> image 객체로 응답

--- src/main/java/container/restaurant/server/domain/restaurant/Restaurant.java
+++ src/main/java/container/restaurant/server/domain/restaurant/Restaurant.java
@@ -2,18 +2,19 @@ package container.restaurant.server.domain.restaurant;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import container.restaurant.server.domain.base.BaseEntity;
+import container.restaurant.server.domain.feed.picture.Image;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.ColumnDefault;
 import org.locationtech.jts.geom.Point;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
 @Getter
 @NoArgsConstructor
-@Entity(name = "tb_restaurant")
+@Entity(name = "TB_RESTAURANT")
 public class Restaurant extends BaseEntity {
 
     @NotNull
@@ -32,19 +33,20 @@ public class Restaurant extends BaseEntity {
     private double longitude;
 
     @NotNull
-    private Long image_ID;
+    @OneToOne
+    private Image image;
 
     @ColumnDefault("0")
-    private int vinishCount;
+    private int vanishCount;
 
     @Builder
-    protected Restaurant(String name, String addr, Point loc, double lon, double lat) {
+    protected Restaurant(String name, String addr, Point loc, double lon, double lat, Image image) {
         this.name = name;
         this.address = addr;
         this.location = loc;
         this.longitude = lon;
         this.latitude = lat;
-        this.image_ID = 0L;
+        this.image = image;
     }
 
 }
