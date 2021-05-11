50eeeaf80ff05262f7047da866c594d9c05f7329
parkjung1129 <dlwfp94@gmail.com>
2021-05-11 17:50:30 +0900

    :sparkles: 식당 정보 DTO 생성
    필요 여부에 따라 삭제될 가능성 있음

---
+++ src/main/java/container/restaurant/server/web/dto/restaurant/RestaurantInfoDto.java
@@ -0,0 +1,30 @@
+package container.restaurant.server.web.dto.restaurant;
+
+import container.restaurant.server.domain.restaurant.Restaurant;
+import lombok.Getter;
+import org.springframework.hateoas.RepresentationModel;
+
+import java.util.Optional;
+
+@Getter
+public class RestaurantInfoDto extends RepresentationModel<RestaurantInfoDto> {
+
+    private final String name;
+    private final String address;
+    private final double latitude;
+    private final double longitude;
+    private final String image_path;
+
+    public static RestaurantInfoDto from(Restaurant restaurant) {
+        return new RestaurantInfoDto(restaurant);
+    }
+
+    protected RestaurantInfoDto(Restaurant restaurant) {
+        this.name = restaurant.getName();
+        this.address = restaurant.getAddress();
+        this.latitude = restaurant.getLatitude();
+        this.longitude = restaurant.getLongitude();
+        this.image_path = restaurant.getImage().getUrl();
+    }
+
+}
