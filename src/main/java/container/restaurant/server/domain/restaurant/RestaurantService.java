51a5f7882c078aa471f0d634ffa08d9e979fbb27
dlwfp <dlwfp94@gmail.com>
2021-05-11 23:23:13 +0900

    :label: 식당 도메인 및 리포 추가
    식당 도메인 추가 / 리포 및 서비스 기능 구현

---
+++ src/main/java/container/restaurant/server/domain/restaurant/RestaurantService.java
@@ -0,0 +1,38 @@
+package container.restaurant.server.domain.restaurant;
+
+import container.restaurant.server.web.dto.restaurant.RestaurantInfoDto;
+import container.restaurant.server.web.dto.restaurant.RestaurantNameInfoDto;
+import container.restaurant.server.web.dto.restaurant.RestaurantNearInfoDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class RestaurantService {
+    private final RestaurantRepository restaurantRepository;
+
+    public RestaurantInfoDto findById(Long id) {
+        return RestaurantInfoDto.from(restaurantRepository.findById(id).get());
+    }
+
+    public List<RestaurantNearInfoDto> findNearByRestaurants(double lat, double lon, long radius) {
+        return restaurantRepository.findNearByRestaurants(lat, lon, radius)
+                .stream()
+                .map(RestaurantNearInfoDto::from)
+                .collect(Collectors.toList());
+    }
+
+    public List<RestaurantNameInfoDto> searchRestaurantName(String name) {
+        return restaurantRepository.searchRestaurantName(name)
+                .stream()
+                .map(RestaurantNameInfoDto::from)
+                .collect(Collectors.toList());
+    }
+
+    public void updateVanish(Long id) {
+        restaurantRepository.updateVanish(id);
+    }
+}
