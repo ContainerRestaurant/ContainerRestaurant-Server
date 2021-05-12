b656da2ccdad588198dbb69eef05f2cb7e88ee90
Cenibee <30590761+Cenibee@users.noreply.github.com>
2021-05-12 14:48:07 +0900

    :label: FeedPreviewDto 속성 이름 수정 및 NonNull 추가

--- src/main/java/container/restaurant/server/web/dto/feed/FeedPreviewDto.java
+++ src/main/java/container/restaurant/server/web/dto/feed/FeedPreviewDto.java
@@ -3,6 +3,7 @@ package container.restaurant.server.web.dto.feed;
 import container.restaurant.server.domain.feed.Feed;
 import lombok.Getter;
 import org.springframework.hateoas.RepresentationModel;
+import org.springframework.lang.NonNull;
 
 /**
  * 피드 리스트를 요청한 경우, 리스트에는 각 피드에 대한 간략한 정보만 응답하기 위한 DTO
@@ -13,11 +14,12 @@ public class FeedPreviewDto extends RepresentationModel<FeedPreviewDto> {
     private final Long id;
 
     private final String thumbnailUrl;
-    private final String nickname;
+    private final String ownerNickname;
     private final String content;
     private final Integer likeCount;
     private final Integer replyCount;
 
+    @NonNull
     public static FeedPreviewDto from(Feed feed) {
         return new FeedPreviewDto(feed);
     }
@@ -26,7 +28,7 @@ public class FeedPreviewDto extends RepresentationModel<FeedPreviewDto> {
         this.id = feed.getId();
 
         this.thumbnailUrl = feed.getThumbnailUrl();
-        this.nickname = feed.getOwner().getNickname();
+        this.ownerNickname = feed.getOwner().getNickname();
         this.content = feed.getContent();
         this.likeCount = feed.getLikeCount();
         this.replyCount = feed.getReplyCount();
