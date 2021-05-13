c966dee506d9d769d3f8de9712849e9d0f589bc5
cenibee <ksj3452@naver.com>
2021-05-13 15:51:53 +0900

    :bug: FeedRepository 관련 다수 수정
    유저는 피드를 가져올 때 항상 참조해야하므로 EAGER 를 사용하고, N+1 문제가 발생하지 않도록 EntityGraph 주석을 적용
    
    레스토랑은 피드 리스트 탐색시에 필요하지 않으므로 Lazy Loading 적용
    
    FeeRepository 에서 엔티티로 탐색하지 않고 ID 로 탐색하도록 수정
    
    FeedRepositoryTest 구조 수정

--- src/main/java/container/restaurant/server/domain/feed/FeedRepository.java
+++ src/main/java/container/restaurant/server/domain/feed/FeedRepository.java
@@ -4,12 +4,21 @@ import container.restaurant.server.domain.restaurant.Restaurant;
 import container.restaurant.server.domain.user.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.NonNull;
 
 public interface FeedRepository extends JpaRepository<Feed, Long> {
 
-    Page<Feed> findAllByOwner(User owner, Pageable pageable);
+    @Override
+    @NonNull
+    @EntityGraph(attributePaths = { "owner" })
+    Page<Feed> findAll(@NonNull Pageable pageable);
 
-    Page<Feed> findAllByRestaurant(Restaurant restaurant, Pageable pageable);
+    @EntityGraph(attributePaths = { "owner" })
+    Page<Feed> findAllByOwnerId(Long userId, Pageable pageable);
+
+    @EntityGraph(attributePaths = { "owner" })
+    Page<Feed> findAllByRestaurantId(Long restaurantId, Pageable pageable);
 
 }
