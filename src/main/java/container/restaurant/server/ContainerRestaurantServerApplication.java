79083e94656a18678fee318dba79ef284977f148
HongYeseul <ghd8119@naver.com>
2021-05-23 18:15:47 +0900

    🔧 약관 - 시작 시 초기화 & 매일 갱신되도록 수정

--- src/main/java/container/restaurant/server/ContainerRestaurantServerApplication.java
+++ src/main/java/container/restaurant/server/ContainerRestaurantServerApplication.java
@@ -2,7 +2,9 @@ package container.restaurant.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class ContainerRestaurantServerApplication {
 
