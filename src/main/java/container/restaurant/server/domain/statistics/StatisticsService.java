1e03b3d980a94c5a85eab6e61865d16af05eb958
Cenibee <30590761+Cenibee@users.noreply.github.com>
2021-05-26 01:34:47 +0900

    :art: WARN 문구 제거
    1. MAX_COUNT 전역 상수로 선언
    
    2. 발생하지 않는 throws 구문 제거
    
    3. 스트림 API 사용 개선
    
    4. EntityModel -> StatisticUserDto 로 형 변경 (StatisticUserDto 가 RepresentationModel 확장이기 때문에 동일한 사용성 제공)
    
    5. LinkedList 제거 로직 개선 -> 링크드 리스트 내부에 없는 데이터를 삭제하면 false 반환하고 아무일이 일어나지 않음

--- src/main/java/container/restaurant/server/domain/statistics/StatisticsService.java
+++ src/main/java/container/restaurant/server/domain/statistics/StatisticsService.java
@@ -11,7 +11,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.hateoas.EntityModel;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -29,7 +28,7 @@ import java.util.stream.Collectors;
 @Service
 @Log4j2
 public class StatisticsService implements ApplicationRunner {
-    private final int MAX_COUNT = 100;
+    private static final int MAX_COUNT = 100;
     private final UserService userService;
     private final FeedRepository feedRepository;
     private LinkedList<User> userLinkedList;
@@ -42,7 +41,7 @@ public class StatisticsService implements ApplicationRunner {
     private Long totalFeed;
 
     @Override
-    public void run(ApplicationArguments args) throws Exception {
+    public void run(ApplicationArguments args) {
         initToDayFeedWriter();
         refreshTotalFeed();
     }
@@ -53,11 +52,7 @@ public class StatisticsService implements ApplicationRunner {
         LocalDateTime from = LocalDateTime.of(LocalDate.now(), LocalTime.MAX);
         // 오늘 작성한 피드 오래된 순으로 저장 가장 마지막이 최신
         userService.findByToDayFeedWriters(to, from)
-                .stream()
-                .map(user -> {
-                    addRecentUser(user);
-                    return null;
-                }).collect(Collectors.toList());
+                .forEach(this::addRecentUser);
     }
 
     private void refreshTotalFeed() {
@@ -66,21 +61,17 @@ public class StatisticsService implements ApplicationRunner {
 
     @Transactional
     public StatisticsInfoDto getRecentFeedUsers() {
-        List<EntityModel> entityModelList = new ArrayList<>();
-        for (int i = 0; i < userLinkedList.size(); i++) {
-            User user = userLinkedList.get(i);
-            entityModelList
-                    .add(EntityModel.of(StatisticsUserDto.from(user))
-                            .add(userLinker.getUserById(user.getId()).withSelfRel(),
-                                    feedLinker.selectUserFeed(user.getId()).withRel("feeds")));
-        }
-        return StatisticsInfoDto.from(entityModelList, todayFeedCount);
+        List<StatisticsUserDto> statisticUserDtoList = userLinkedList.stream()
+                .map(user -> StatisticsUserDto.from(user).add(
+                        userLinker.getUserById(user.getId()).withSelfRel(),
+                        feedLinker.selectUserFeed(user.getId()).withRel("feeds")))
+                .collect(Collectors.toList());
+        return StatisticsInfoDto.from(statisticUserDtoList, todayFeedCount);
     }
 
     public void addRecentUser(User user) {
         // 중복되는 사용자가 있으면, 해당 위치를 지우고 최신으로 추가
-        if (userLinkedList.contains(user))
-            userLinkedList.remove(user);
+        userLinkedList.remove(user);
 
         // 리스트 사용자가 n 명이 넘으면 마지막 삭제 LRU 형식
         if (userLinkedList.size() >= MAX_COUNT)
