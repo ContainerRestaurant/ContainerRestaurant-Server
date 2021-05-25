b785270e409778330d21ca3ee98adacd1087f3fe
dlwfp <dlwfp94@gmail.com>
2021-05-25 23:04:58 +0900

    :sparkles: 푸시 이벤트 처리 구현
    푸시 이벤트 처리 구현
    상세한 내용은 노션 참조

---
+++ src/main/java/container/restaurant/server/utils/FirebaseCloudMessageUtils.java
@@ -0,0 +1,82 @@
+package container.restaurant.server.utils;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.auth.oauth2.GoogleCredentials;
+import container.restaurant.server.domain.push.FcmMessage;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import okhttp3.*;
+import org.apache.http.HttpHeaders;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.List;
+
+@Component
+@Log4j2
+@RequiredArgsConstructor
+public class FirebaseCloudMessageUtils {
+
+    @Value("${google.push.api.url}")
+    private String API_URL;
+
+    @Value("${server.real.base.url}")
+    private String IMAGE_DEFAULT;
+    private final String TEMP_IMAGE = "/api/image/ebadca0e-30ac-58e3-914c-5303fc4c6061.jpeg";
+
+    private final ObjectMapper objectMapper;
+
+    public void sendMessage(String target, String title, String body) throws IOException {
+        String message = makeMessage(target, title, body);
+
+        OkHttpClient client = new OkHttpClient();
+        RequestBody requestBody = RequestBody.create(message, MediaType.get("application/json; charset=utf-8"));
+        Request request = new Request.Builder()
+                .url(API_URL)
+                .post(requestBody)
+                .addHeader(HttpHeaders.AUTHORIZATION, "Bearer " + createAccessToken())
+                .addHeader(HttpHeaders.CONTENT_TYPE, "application/json; UTF-8")
+                .build();
+
+        Response response = client.newCall(request).execute();
+
+        if (response.code() == 200) {
+            log.info("title => [ " + title + " ] Push  Success");
+        } else {
+            log.info("Push Failed Code => " + response.code());
+        }
+    }
+
+    private String makeMessage(String target, String title, String body) throws JsonProcessingException {
+        FcmMessage fcmMessage = FcmMessage.builder()
+                .message(FcmMessage.Message.builder()
+                        .token(target)
+                        .notification(FcmMessage.Notification.builder()
+                                .title(title)
+                                .body(body)
+                                .image(null)
+//                                .image(IMAGE_DEFAULT + TEMP_IMAGE)
+                                .build()
+                        )
+                        .build()
+                )
+                .validate_only(false)
+                .build();
+        return objectMapper.writeValueAsString(fcmMessage);
+    }
+
+    private String createAccessToken() throws IOException {
+        String firebaseConfigPath = "firebase_service_key.json";
+
+        GoogleCredentials googleCredentials = GoogleCredentials
+                .fromStream(new ClassPathResource(firebaseConfigPath).getInputStream())
+                .createScoped(List.of("https://www.googleapis.com/auth/cloud-platform"));
+
+        googleCredentials.refreshIfExpired();
+        return googleCredentials.getAccessToken().getTokenValue();
+
+    }
+}
