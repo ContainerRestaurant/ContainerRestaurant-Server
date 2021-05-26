d0cee67cb4921e73f298210863d28e1a4bb64a8c
HongYeseul <ghd8119@naver.com>
2021-05-26 10:49:20 +0900

    ✨ 배너 로직 구현에 필요한 클래스 추가

---
+++ src/main/java/container/restaurant/server/domain/banner/Banner.java
@@ -0,0 +1,15 @@
+package container.restaurant.server.domain.banner;
+
+import container.restaurant.server.domain.base.BaseEntity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Getter
+@NoArgsConstructor
+@Entity(name = "TB_BANNER")
+public class Banner extends BaseEntity {
+    private String title;
+    private String url;
+}
---
+++ src/main/java/container/restaurant/server/domain/banner/BannerRepository.java
@@ -0,0 +1,6 @@
+package container.restaurant.server.domain.banner;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BannerRepository extends JpaRepository<Banner, Long> {
+}
