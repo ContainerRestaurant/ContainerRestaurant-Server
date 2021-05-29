a7c6b8ab7e8f8f28177f7da003e5e11bcec198bd
Cenibee <30590761+Cenibee@users.noreply.github.com>
2021-05-30 01:21:37 +0900

    :ambulance: 배포 완료시, 이전에 동작하던 서버 즉시 종료
    현재 프리티어에서 메모리 과다 문제가 있어 무중단 배포 완료시 이전 서버를 즉시 종료

--- scripts/test-deploy/switch.sh
+++ scripts/test-deploy/switch.sh
@@ -13,4 +13,20 @@ function switch_proxy() {
 
   echo "> 엔진엑스 Reload"
   sudo service nginx reload
+  sleep 5
+
+  # 현재 프리티어에서 두 개의 서버를 띄워놓으면 메모리를 너무 많이 먹어 동작이 안되는 경우가 있어 기존 서버를 종료한다.
+  echo "> 기존에 동작하던 서버를 종료합니다."
+
+  if [ ${IDLE_PORT} == 8081 ]
+  then
+    NEW_IDLE_PORT=8082
+  else
+    NEW_IDLE_PORT=8081
+  fi
+  echo "> 종료할 서버의 포트: $NEW_IDLE_PORT"
+
+  NEW_IDLE_PID=$(lsof -ti tcp:${NEW_IDLE_PORT})
+  echo "> kill -15"
+  kill -15 $NEW_IDLE_PID
 }
