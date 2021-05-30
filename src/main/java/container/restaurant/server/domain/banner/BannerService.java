ca3f39fc804452147db771dd74783591b7c6091e
HongYeseul <ghd8119@naver.com>
2021-05-30 11:13:35 +0900

    :recycle: 배너 링크들을 주기적으로 업데이트하도록 수정 - 통신 할 때 마다 DB 검색 -> 서버 켜지거나 새벽 2시마다 배너 리스트에 업데이트하도록 수정

--- src/main/java/container/restaurant/server/domain/banner/BannerService.java
+++ src/main/java/container/restaurant/server/domain/banner/BannerService.java
@@ -4,9 +4,11 @@ import container.restaurant.server.web.dto.banner.BannerInfoDto;
 import container.restaurant.server.web.linker.ImageLinker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.hateoas.CollectionModel;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,15 +17,16 @@ import java.util.List;
 public class BannerService {
     private final BannerRepository bannerRepository;
     private final ImageLinker imageLinker;
+    List<BannerInfoDto> banners = new ArrayList<>();
 
-    @Transactional
-    public CollectionModel<BannerInfoDto> getBanners(){
-        List<BannerInfoDto> banners = new ArrayList<>();
+    @PostConstruct
+    @Scheduled(cron = "0 0 2 * * *")
+    public void putBanners(){
         String baseURL = imageLinker.getImage("").toString();
-
         bannerRepository.findAll().forEach(banner ->
-                banners.add(BannerInfoDto.from(banner, baseURL))
-        );
-        return CollectionModel.of(banners);
+                banners.add(BannerInfoDto.from(banner, baseURL)));
     }
+
+    @Transactional(readOnly = true)
+    public CollectionModel<BannerInfoDto> getBanners(){ return CollectionModel.of(banners); }
 }
