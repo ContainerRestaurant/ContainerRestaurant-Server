f2594d6d922d2ae0f31fc53639cb036bbd1df07a
parkjung1129 <dlwfp94@gmail.com>
2021-06-02 15:43:13 +0900

    :white_check_mark: 푸시 토큰 관련 테스트코드 수정

--- src/test/java/container/restaurant/server/web/UserControllerTest.java
+++ src/test/java/container/restaurant/server/web/UserControllerTest.java
@@ -143,7 +143,8 @@ class UserControllerTest extends BaseUserControllerTest {
                 .andDo(document("patch-user",
                         requestFields(
                                 fieldWithPath("nickname").description("변경할 닉네임"),
-                                fieldWithPath("profile").description("변경할 프로필 사진 경로")
+                                fieldWithPath("profile").description("변경할 프로필 사진 경로"),
+                                fieldWithPath("pushToken").description("변경할 푸시 토큰 아이디")
                         )));
     }
 
