096c41bcf0ddbafc48fd5bfe72b650b190ef620b
Cenibee <30590761+Cenibee@users.noreply.github.com>
2021-06-24 01:27:08 +0900

    :memo: 배너 Docs 추가

--- src/main/java/container/restaurant/server/domain/home/banner/Banner.java
+++ src/main/java/container/restaurant/server/domain/home/banner/Banner.java
@@ -1,13 +1,17 @@
 package container.restaurant.server.domain.home.banner;
 
 import container.restaurant.server.domain.base.BaseEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 
 @Getter
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
 @Entity(name = "TB_BANNER")
 public class Banner extends BaseEntity {
     private String title;
