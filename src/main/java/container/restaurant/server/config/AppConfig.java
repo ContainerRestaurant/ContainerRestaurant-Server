a4be71beb0c95756c947ba6e3886cf7a66a5bbc4
Cenibee <30590761+Cenibee@users.noreply.github.com>
2021-06-26 20:30:38 +0900

    :loud_sound: HTTP 요청 로깅을 위해 Actuator 추가
    :hammer: 기본 HTTP 추적 리파지토리 추가
    
    - 일단 인메모리로 추가했습니다.
    
    :heavy_plus_sign: boot-starter-actuator 의존성 추가

--- src/main/java/container/restaurant/server/config/AppConfig.java
+++ src/main/java/container/restaurant/server/config/AppConfig.java
@@ -1,6 +1,8 @@
 package container.restaurant.server.config;
 
 import container.restaurant.server.process.oauth.OAuthAgentFactory;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -14,6 +16,11 @@ public class AppConfig {
     @Bean
     public OAuthAgentFactory getOAuthAgentFactory() {
         return OAuthAgentFactory.createDefaultFactory();
+    }
+
+    @Bean
+    public HttpTraceRepository getHttpTraceRepository() {
+        return new InMemoryHttpTraceRepository();
     }
 
 }
