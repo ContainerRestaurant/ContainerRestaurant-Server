90ad612f8d5b67a794231e6ea5480e9160de0f21
cenibee <ksj3452@naver.com>
2021-07-26 18:37:38 +0900

    :hammer: 400 에러 핸들러 수정
    - 단일 참조 메서드를 인라인 리팩토링
    - Repository.findByXxx() 메서드에 비정상 null 이 들어온 경우 추가

--- src/main/java/container/restaurant/server/exception/ExceptionAdvice.java
+++ src/main/java/container/restaurant/server/exception/ExceptionAdvice.java
@@ -1,9 +1,5 @@
 package container.restaurant.server.exception;
 
-import container.restaurant.server.exception.UnauthorizedException;
-import container.restaurant.server.exception.UsingPushTokenException;
-import container.restaurant.server.exception.ResourceNotFoundException;
-import container.restaurant.server.exception.FailedAuthorizationException;
 import container.restaurant.server.web.dto.ErrorDto;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.http.HttpStatus;
@@ -33,6 +29,13 @@ public class ExceptionAdvice {
 
     @ExceptionHandler(ConstraintViolationException.class)
     public ResponseEntity<?> constraintViolationException(ConstraintViolationException e) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(EntityModel.of(ErrorDto.from(e)));
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<?> constraintViolationException(IllegalArgumentException e) {
         return response(HttpStatus.BAD_REQUEST, e);
     }
 
@@ -45,14 +48,6 @@ public class ExceptionAdvice {
         return ResponseEntity
                 .status(status)
                 .body(EntityModel.of(ErrorDto.from(exception)));
-    }
-
-    private ResponseEntity<EntityModel<ErrorDto>> response(
-            HttpStatus status, ConstraintViolationException validException
-    ) {
-        return ResponseEntity
-                .status(status)
-                .body(EntityModel.of(ErrorDto.from(validException)));
     }
 
 }
