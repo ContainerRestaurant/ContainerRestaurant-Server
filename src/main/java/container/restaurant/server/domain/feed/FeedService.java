f207ff4dee901dd0515b69681e06cb9565bdf219
Cenibee <30590761+Cenibee@users.noreply.github.com>
2021-09-26 21:47:13 +0900

    :zap: 피드 리스트 조회시 좋아요/스크랩 관련 쿼리 개선

--- src/main/java/container/restaurant/server/domain/feed/FeedService.java
+++ src/main/java/container/restaurant/server/domain/feed/FeedService.java
@@ -19,11 +19,13 @@ import container.restaurant.server.web.dto.feed.FeedDetailDto;
 import container.restaurant.server.web.dto.feed.FeedInfoDto;
 import container.restaurant.server.web.dto.feed.FeedPreviewDto;
 import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PagedResourcesAssembler;
 import org.springframework.hateoas.PagedModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -31,6 +33,10 @@ import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static java.util.Optional.ofNullable;
 
@@ -75,48 +81,44 @@ public class FeedService {
 
     @Transactional(readOnly = true)
     public PagedModel<FeedPreviewDto> findAll(Pageable pageable, Category categoryFilter, Long loginId) {
-        return feedAssembler.toModel(
-                ofNullable(categoryFilter)
-                        .map(category -> feedRepository.findAllByCategory(pageable, categoryFilter))
-                        .orElseGet(() -> feedRepository.findAll(pageable)),
-                feed -> createFeedPreviewDto(feed, loginId)
-        );
+        Page<Feed> page = categoryFilter != null ?
+                feedRepository.findAllByCategory(pageable, categoryFilter) :
+                feedRepository.findAll(pageable);
+
+        return feedAssembler.toModel(page, new FeedPreviewDtoAssembler(loginId, page));
     }
 
     @Transactional(readOnly = true)
     public PagedModel<FeedPreviewDto> findAllByUser(
-            Long userId, Long loginId, Pageable pageable, Category categoryFilter) {
-        return feedAssembler.toModel(
-                ofNullable(categoryFilter)
-                        .map(category -> feedRepository.findAllByOwnerIdAndCategory(
-                                userId, pageable, categoryFilter))
-                        .orElseGet(() -> feedRepository.findAllByOwnerId(userId, pageable)),
-                feed -> createFeedPreviewDto(feed, loginId)
-        );
+            Long userId, Long loginId, Pageable pageable, Category categoryFilter
+    ) {
+        Page<Feed> page = categoryFilter != null ?
+                feedRepository.findAllByOwnerIdAndCategory(userId, pageable, categoryFilter) :
+                feedRepository.findAllByOwnerId(userId, pageable);
+
+        return feedAssembler.toModel(page, new FeedPreviewDtoAssembler(loginId, page));
     }
 
     @Transactional(readOnly = true)
     public PagedModel<FeedPreviewDto> findAllByRestaurant(
-            Long restaurantId, Long loginId, Pageable pageable, Category categoryFilter) {
-        return feedAssembler.toModel(
-                ofNullable(categoryFilter)
-                        .map(category -> feedRepository.findAllByRestaurantIdAndCategory(
-                                restaurantId, pageable, categoryFilter))
-                        .orElseGet(() -> feedRepository.findAllByRestaurantId(restaurantId, pageable)),
-                feed -> createFeedPreviewDto(feed, loginId)
-        );
+            Long restaurantId, Long loginId, Pageable pageable, Category categoryFilter
+    ) {
+        Page<Feed> page = categoryFilter != null ?
+                feedRepository.findAllByRestaurantIdAndCategory(restaurantId, pageable, categoryFilter) :
+                feedRepository.findAllByRestaurantId(restaurantId, pageable);
+
+        return feedAssembler.toModel(page, new FeedPreviewDtoAssembler(loginId, page));
     }
 
     @Transactional(readOnly = true)
     public PagedModel<FeedPreviewDto> findAllByUserScrap(
-            Long userId, Long loginId, Pageable pageable, Category categoryFilter) {
-        return feedAssembler.toModel(
-                ofNullable(categoryFilter)
-                        .map(category -> feedRepository.findAllByScraperIdAndCategory(
-                                userId, pageable, categoryFilter))
-                        .orElseGet(() -> feedRepository.findAllByScraperId(userId, pageable)),
-                feed -> createFeedPreviewDto(feed, loginId)
-        );
+            Long userId, Long loginId, Pageable pageable, Category categoryFilter
+    ) {
+        Page<Feed> page = categoryFilter != null ?
+                feedRepository.findAllByScraperIdAndCategory(userId, pageable, categoryFilter) :
+                feedRepository.findAllByScraperId(userId, pageable);
+
+        return feedAssembler.toModel(page, new FeedPreviewDtoAssembler(loginId, page));
     }
 
     @Transactional
@@ -205,11 +207,28 @@ public class FeedService {
         return feedRepository.findAllByCreatedDateBetweenOrderByCreatedDateDesc(from, to, pageable);
     }
 
-    @Transactional(readOnly = true)
-    public FeedPreviewDto createFeedPreviewDto(Feed feed, Long loginId) {
-        return FeedPreviewDto.from(
-                feed,
-                feedLikeRepository.existsByUserIdAndFeedId(loginId, feed.getId()),
-                scrapFeedRepository.existsByUserIdAndFeedId(loginId, feed.getId()));
+    private class FeedPreviewDtoAssembler implements RepresentationModelAssembler<Feed, FeedPreviewDto> {
+
+        final Set<Long> likeIdSet;
+        final Set<Long> scrapIdSet;
+
+        public FeedPreviewDtoAssembler(Long loginId, Supplier<Stream<Feed>> feedSupplier) {
+            List<Long> feedIdList = feedSupplier.get()
+                    .map(Feed::getId)
+                    .collect(Collectors.toList());
+            likeIdSet = loginId == null ? Set.of() :
+                    feedLikeRepository.checkFeedLikeOnIdList(loginId, feedIdList);
+            scrapIdSet = loginId == null ? Set.of() :
+                    scrapFeedRepository.checkScrapFeedOnIdList(loginId, feedIdList);
+        }
+
+        @NotNull
+        @Override
+        public FeedPreviewDto toModel(@NotNull Feed entity) {
+            return FeedPreviewDto.from(
+                    entity,
+                    likeIdSet.contains(entity.getId()),
+                    scrapIdSet.contains(entity.getId()));
+        }
     }
 }
