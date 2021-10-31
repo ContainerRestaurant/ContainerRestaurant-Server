73af547e010431cffd88416ee975dc2dc7530a6b
Cenibee <30590761+Cenibee@users.noreply.github.com>
2021-10-31 22:49:26 +0900

    :bug: 용기 레벨 변경

--- src/main/java/container/restaurant/server/domain/user/ContainerLevel.java
+++ src/main/java/container/restaurant/server/domain/user/ContainerLevel.java
@@ -8,7 +8,7 @@ import lombok.RequiredArgsConstructor;
 public enum ContainerLevel {
     LEVEL_1("LV1. 텀블러"),
     LEVEL_2("LV2. 밥그릇"),
-    LEVEL_3("LV3. 금 용기"),
+    LEVEL_3("LV3. 용기 세트"),
     LEVEL_4("LV4. 후라이팬"),
     LEVEL_5("LV5. 냄비");
 
