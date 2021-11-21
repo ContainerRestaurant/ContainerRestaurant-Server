6ede2444a6a40d450458acf049130071446ced57
Cenibee <30590761+Cenibee@users.noreply.github.com>
2021-11-21 21:10:56 +0900

    :bug: 찜한 식당 개수가 업데이트 되지 않는 문제 해결

--- src/main/java/container/restaurant/server/web/dto/feed/LevelUpDto.java
+++ src/main/java/container/restaurant/server/web/dto/feed/LevelUpDto.java
@@ -2,15 +2,22 @@ package container.restaurant.server.web.dto.feed;
 
 import container.restaurant.server.domain.user.ContainerLevel;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+
+import static container.restaurant.server.domain.user.ContainerLevel.getLevel;
 
 @Getter
-@RequiredArgsConstructor
 public class LevelUpDto {
     private final Integer levelFeedCount;
+    private final String from;
     private final String to;
 
+    private LevelUpDto(ContainerLevel to) {
+        this.levelFeedCount = to.getNeedCount();
+        this.from = getLevel(to.getLevel() - 1).getTitle();
+        this.to = to.getTitle();
+    }
+
     public static LevelUpDto to(ContainerLevel to) {
-        return new LevelUpDto(to.getNeedCount(), to.getTitle());
+        return new LevelUpDto(to);
     }
 }
