50e4158d08f25db9ff3de7dd801464ed76c6b75a
Cenibee <30590761+Cenibee@users.noreply.github.com>
2022-01-09 20:42:50 +0900

    :sparkles: 피드상세 API 에서 식당의 위도 경도 추가 - import 문 제거

--- src/main/java/container/restaurant/server/domain/restaurant/RestaurantRepository.java
+++ src/main/java/container/restaurant/server/domain/restaurant/RestaurantRepository.java
@@ -8,7 +8,6 @@ import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
