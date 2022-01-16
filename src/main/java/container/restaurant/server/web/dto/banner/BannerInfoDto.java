f799aaa687127510eb66936ffbb5d0ff206ab907
hellozin <paul26375@gmail.com>
2022-01-16 13:52:12 +0900

    :ambulance: banner URL 에 host 임시로 추가

--- src/main/java/container/restaurant/server/web/dto/banner/BannerInfoDto.java
+++ src/main/java/container/restaurant/server/web/dto/banner/BannerInfoDto.java
@@ -10,11 +10,15 @@ public class BannerInfoDto {
     private final String contentURL;
     private final String additionalURL;
 
+    // TODO: 로직 확인하고 제거
+    private static final String HOST = "http://ec2-52-78-66-184.ap-northeast-2.compute.amazonaws.com";
+
     public static BannerInfoDto from(Banner banner, String baseURL) {
         return new BannerInfoDto(banner, baseURL);
     }
 
     protected BannerInfoDto(Banner banner, String baseURL) {
+        baseURL = HOST + baseURL;
         this.title = banner.getTitle();
         this.bannerURL = baseURL + banner.getBannerUrl();
         this.contentURL = banner.getContentUrl() != null ?
