d2a5c092325af8b6e20d33cb98f64edf5bb01aa1
hellozin <paul26375@gmail.com>
2022-03-15 19:44:27 +0900

    [feature-46] 푸시 토큰 요청은 비로그인 허용, api path 명확하게 수정

--- src/main/java/container/restaurant/server/web/PushController.java
+++ src/main/java/container/restaurant/server/web/PushController.java
@@ -3,42 +3,19 @@ package container.restaurant.server.web;
 import container.restaurant.server.config.auth.LoginId;
 import container.restaurant.server.domain.push.PushToken;
 import container.restaurant.server.domain.push.PushTokenService;
-import container.restaurant.server.utils.FirebaseCloudMessageUtils;
 import container.restaurant.server.web.linker.PushTokenLinker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.hateoas.EntityModel;
-import org.springframework.hateoas.mediatype.hal.HalModelBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.io.IOException;
-
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/push")
 public class PushController {
 
-    final private FirebaseCloudMessageUtils firebaseCloudMessageUtils;
     final private PushTokenService pushTokenService;
     final private PushTokenLinker pushTokenLinker;
-
-    /*
-     * 푸시 테스트용 불필요시 삭제 예정
-     *
-     * @param token
-     * @return
-     * @throws IOException
-     */
-    @GetMapping(value = "{token}")
-    public ResponseEntity<?> pushSendTest(@PathVariable() String token) throws IOException {
-        PushToken pushToken = PushToken.builder().token(token).build();
-        firebaseCloudMessageUtils.sendMessage(pushToken, "용기낸 식당", "Push 알림 테스트 입니다.");
-        return ResponseEntity.ok(
-                HalModelBuilder.emptyHalModel().build()
-                        .add(linkTo(PushController.class).withSelfRel()));
-    }
 
     /*
      * [ 비로그인 허용 ]
@@ -50,7 +27,7 @@ public class PushController {
      * @param token
      * @return
      */
-    @PostMapping("{token}")
+    @PostMapping("/token/{token}")
     public ResponseEntity<?> registerClientPushToken(
             @LoginId Long loginId,
             @PathVariable String token) {
@@ -71,7 +48,7 @@ public class PushController {
      * @param sessionUser
      * @return
      */
-    @DeleteMapping("{tokenId}")
+    @DeleteMapping("/token/{tokenId}")
     public ResponseEntity<?> deleteClientPushToken(
             @LoginId Long loginId,
             @PathVariable Long tokenId) {
